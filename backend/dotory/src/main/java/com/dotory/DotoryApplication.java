2360a368662559b1c1658614566844686c70edc5
여희림 <dugmlfla0219@gmail.com>
2026-05-22 22:26:06 +0900

    chore: 백엔드 프로젝트 생성

---
+++ backend/dotory/src/main/java/com/dotory/DotoryApplication.java
@@ -0,0 +1,13 @@
+package com.dotory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotoryApplication.class, args);
+	}
+
+}
---
+++ backend/dotory/src/test/java/com/dotory/DotoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dotory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
